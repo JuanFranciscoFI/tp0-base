427c7e06fae77ff3ddd7e66c334c5cf73b49f983
Juan Francisco <jcuevas@fi.uba.ar>
2025-08-31 16:15:46 -0300

    feat: logic

--- client/common/client.go
+++ client/common/client.go
@@ -1,8 +1,6 @@
 package common
 
 import (
-	"bufio"
-	"fmt"
 	"net"
 	"time"
 
@@ -23,6 +21,7 @@ type ClientConfig struct {
 type Client struct {
 	config ClientConfig
 	conn   net.Conn
+	protocol *Protocol
 }
 
 // NewClient Initializes a new client receiving the configuration
@@ -30,6 +29,7 @@ type Client struct {
 func NewClient(config ClientConfig) *Client {
 	client := &Client{
 		config: config,
+		protocol: nil,
 	}
 	return client
 }
@@ -40,52 +40,33 @@ func NewClient(config ClientConfig) *Client {
 func (c *Client) createClientSocket() error {
 	conn, err := net.Dial("tcp", c.config.ServerAddress)
 	if err != nil {
-		log.Criticalf(
-			"action: connect | result: fail | client_id: %v | error: %v",
-			c.config.ID,
-			err,
-		)
+		log.Criticalf("action: connect | result: fail | client_id: %v | error: %v", c.config.ID, err)
+		return err
 	}
 	c.conn = conn
+	c.protocol = NewProtocol(conn)
 	return nil
 }
 
 // StartClientLoop Send messages to the client until some time threshold is met
-func (c *Client) StartClientLoop() {
-	// There is an autoincremental msgID to identify every message sent
-	// Messages if the message amount threshold has not been surpassed
-	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
-		// Create the connection the server in every loop iteration. Send an
-		c.createClientSocket()
-
-		// TODO: Modify the send to avoid short-write
-		fmt.Fprintf(
-			c.conn,
-			"[CLIENT %v] Message N°%v\n",
-			c.config.ID,
-			msgID,
-		)
-		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-		c.conn.Close()
-
-		if err != nil {
-			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-				c.config.ID,
-				err,
-			)
-			return
-		}
-
-		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-			c.config.ID,
-			msg,
-		)
-
-		// Wait a time between sending one message and the next one
-		time.Sleep(c.config.LoopPeriod)
-
+func (c *Client) StartClientLoop(bet Bet) {
+	if err := c.createClientSocket(); err != nil {
+		log.Fatalf("Error al crear el socket: %v", err)
 	}
-	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+	defer c.protocol.Close()
+
+	if err := c.protocol.SendBet(bet); err != nil {
+		log.Errorf("action: apuesta_enviada | result: fail | error: %v", err)
+		return
+	}
+
+	_, err := c.protocol.RecvResponse()
+	if err != nil {
+		log.Errorf("action: apuesta_enviada | result: fail | error: %v", err)
+		return
+	}
+
+	log.Infof("action: apuesta_enviada | result: success | dni: %v | numero: %v", bet.Documento, bet.Numero)
 }
 
 // Close the client socket
--- server/common/server.py
+++ server/common/server.py
@@ -1,15 +1,16 @@
 import socket
 import logging
 import signal
-import sys
+from common.protocol import Protocol
+from common.utils import store_bets
 
 class Server:
-    def __init__(self, port, listen_backlog):
-        # Initialize server socket
+    def __init__(self, port, listen_backlog, agency_id=0):
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
         self._shutdown = False
+        self._agency_id = agency_id
 
     def __graceful_shutdown(self, signum, frame):
         logging.info("action: graceful_shutdown | result: in_progress")
@@ -18,16 +19,6 @@ class Server:
         logging.info("action: graceful_shutdown | result: success")
 
     def run(self):
-        """
-        Dummy Server loop
-
-        Server that accept a new connections and establishes a
-        communication with a client. After client with communucation
-        finishes, servers starts to accept new connections again
-        """
-
-        # TODO: Modify this program to handle signal to graceful shutdown
-        # the server
         signal.signal(signal.SIGINT, self.__graceful_shutdown)
         signal.signal(signal.SIGTERM, self.__graceful_shutdown)
 
@@ -40,38 +31,29 @@ class Server:
                 except OSError as e:
                     if not self._shutdown:
                         logging.error(f"action: accept_connection | error: {str(e)}")
-                    continue
         finally:
             self._server_socket.close()
 
     def __handle_client_connection(self, client_sock):
-        """
-        Read message from a specific client socket and closes the socket
-
-        If a problem arises in the communication with the client, the
-        client socket will also be closed
-        """
+        proto = Protocol(client_sock, self._agency_id)
         try:
-            # TODO: Modify the receive to avoid short-reads
-            msg = client_sock.recv(1024).rstrip().decode('utf-8')
-            addr = client_sock.getpeername()
-            logging.info(f'action: receive_message | result: success | ip: {addr[0]} | msg: {msg}')
-            # TODO: Modify the send to avoid short-writes
-            client_sock.send("{}\n".format(msg).encode('utf-8'))
-        except OSError as e:
-            logging.error("action: receive_message | result: fail | error: {e}")
+            bet = proto.recv_bet()
+            store_bets([bet])
+            proto.send_response(ok=True)
+            logging.info(
+                f"action: apuesta_almacenada | result: success | dni: {bet.document} | numero: {bet.number}"
+            )
+
+        except Exception as e:
+            logging.error(f"action: apuesta_almacenada | result: fail | error: {e}")
+            try:
+                proto.send_response(ok=False)
+            except Exception:
+                pass
         finally:
             client_sock.close()
 
     def __accept_new_connection(self):
-        """
-        Accept new connections
-
-        Function blocks until a connection to a client is made.
-        Then connection created is printed and returned
-        """
-
-        # Connection arrived
         logging.info('action: accept_connections | result: in_progress')
         c, addr = self._server_socket.accept()
         logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
